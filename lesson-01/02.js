9ba2d8830310c3e9ad730a61e24503b4953f4155
Alexander <makedonski_rulez@mail.ru>
2023-10-14 17:27:35 +0300

    lesson 1 - 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,8 @@
 // code
+let number = Math.floor(Math.random() * 100)
+
+if (number % 2 === 0) {
+   console.log(number + ' это чётное число');
+} else {
+   console.log(number + ' это нечётное число');
+}
