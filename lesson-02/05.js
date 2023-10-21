6b9e0d4785bf1b7f272abd3d85d89e8b637f511f
Alexander <makedonski_rulez@mail.ru>
2023-10-21 14:15:11 +0300

    rename console.log

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -7,5 +7,5 @@ let passport = {
 let newPassport = Object.assign({}, passport)
 newPassport.name = 'Ivan';
 
-console.log(passport);
-console.log(newPassport);
+console.log(passport.name);
+console.log(newPassport.name);
