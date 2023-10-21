c5cb0445bec8e98e6becd68ec2a4d48b886cf866
Alexander <makedonski_rulez@mail.ru>
2023-10-21 14:09:25 +0300

    lesson 2-5

---
+++ lesson-02/05.js
@@ -0,0 +1,11 @@
+// code
+let passport = {
+   name: "Petr",
+   surname: "Petrov",
+};
+
+let newPassport = Object.assign({}, passport)
+newPassport.name = 'Ivan';
+
+console.log(passport);
+console.log(newPassport);
