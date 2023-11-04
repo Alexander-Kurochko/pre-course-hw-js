308a63cadba76cecc2c0c2739d21ee7cfe7f613f
Alexander <makedonski_rulez@mail.ru>
2023-11-04 18:11:17 +0300

    lesson 3-7

---
+++ lesson-03/07.js
@@ -0,0 +1,11 @@
+// code
+const numbers = [
+   [1, 2, 3, 4, 5],
+   [11, 20, 33, 40, 55],
+   [111, 200, 333, 400, 555],
+];
+
+
+const numbersDel = numbers.map(el => el.filter(elem => elem % 2 === 0))
+
+console.log(numbersDel)
